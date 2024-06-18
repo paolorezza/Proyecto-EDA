e7c460aa8970492d5bae5258776eeb9c9664a1c8
paolo <paolo@Paolo>
2024-06-18 16:13:31 -0500

    Nuevas clases: Administrador, RegistroAdministrador y su IU

---
+++ src/app/Administrador.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app;
+
+/**
+ *
+ * @author paolo
+ */
+public class Administrador {
+    protected String usuario;
+    protected String contra;
+
+    public Administrador() {
+        this.usuario = null;
+        this.contra = null;
+    }
+    public Administrador(String usuario, String contra) {
+        this.usuario = usuario;
+        this.contra = contra;
+    }
+    
+    
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContra() {
+        return contra;
+    }
+
+    public void setContra(String contra) {
+        this.contra = contra;
+    }
+
+    @Override
+    public String toString() {
+        return "Administrador{" + "usuario=" + usuario + ", contra=" + contra + '}';
+    }
+    
+    
+    
+    
+    
+}
---
+++ src/app/RegistroAdministrador.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app;
+
+import tda.Cola;
+
+/**
+ *
+ * @author paolo
+ */
+public class RegistroAdministrador {
+    private Cola<Administrador> Cola;
+
+    public RegistroAdministrador() {
+        this.Cola = Cola;
+    }
+    
+    
+    public void obtenerInscritos(){
+        
+    }
+}
